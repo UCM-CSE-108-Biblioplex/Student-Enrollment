51ccbe17d7f1bf535b5d033fe670709e5877360d
Luke Henderson <personal@lukeh.us>
2025-04-01 15:20:34 -0700

    added roles, updated models.py

--- website/__init__.py
+++ website/__init__.py
@@ -23,12 +23,14 @@ def start():
     from .site_auth import site_auth
     from .site_enrollment import site_enrollment
     from .site_main import site_main
+    from .site_admin import site_admin
 
     app.register_blueprint(api_main, url_prefix="/api/v1")
 
     app.register_blueprint(site_auth, url_prefix="/auth")
     app.register_blueprint(site_enrollment, url_prefix="/enrollment")
     app.register_blueprint(site_main, url_prefix="/")
+    app.register_blueprint(site_admin, url_prefix="/admin")
 
     from .models import User
 
@@ -47,6 +49,41 @@ def start():
 def create_database(app):
     if(os.path.exists(os.path.join("./instance/", DB_NAME))):
         return
+    
     with app.app_context():
         db.create_all()
+
+        from .models import Role, User
+
+        # student, instructor, admin roles
+        student_role = Role(name="Student")
+        db.session.add(student_role)
+        instructor_role = Role(name="Instructor")
+        db.session.add(instructor_role)
+        ta_role = Role(name="TA")
+        db.session.add(ta_role)
+        admin_role = Role(name="Admin")
+        db.session.add(admin_role)
+
+        # user-defined roles
+        # in case of unforeseen use cases (e.g., Learning Assistent, etc.)
+        defined_roles = os.getenv("EXTRA_ROLES", None)
+        if(defined_roles):
+            defined_roles = defined_roles.split(",")
+            for role in defined_roles:
+                new_role = Role(name=role)
+                db.session.add(new_roll)
+
+        admin_iser = User(
+            username=os.environ.get("DEFAULT_ADMIN_USERNAME", "admin"),
+            password=generate_password_hash(
+                os.environ.get("DEFAULT_ADMIN_PASSWORD", "password"),
+                method="pbkdf2",
+                role=admin_role
+            ),
+
+        )
+
+        db.session.commit()
+
         print("Created Database")
--- website/models.py
+++ website/models.py
@@ -9,9 +9,34 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(255))
     password = db.Column(db.String(255), nullable=False, info={"min_length": 4})
+    api_keys = db.relationship("APIKey", backref="user")
+    courses = db.relationship("Course", secondary="roles", back_populates="users")
+
+class Course(db.Model):
+    __tablename__ = "courses"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    dept = db.Column(db.String(10), nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+    users = db.relationship(
+        "User", secondary="roles", back_populates="courses"
+    )
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(63, nullable=False))
+
+roles = db.Table(
+    "roles",
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column("course_id", db.Integer, db.ForeignKey("courses.id"), primary_key=True),
+    db.Column("role_id", db.Integer, db.ForeignKey("roles.id"))
+)
 
 class APIKey(db.Model):
     __tablename__ = "keys"
     id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, db.ForeignKey("users.id"))
     key = db.Column(db.String(255), nullable=False)
     expiry = db.Column(db.Integer, nullable=False)
