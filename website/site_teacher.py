2480418633bc900aa00ea8426aeb6b712aefab31
Adentratu <sgcremer14@gmail.com>
2025-04-15 03:51:56 -0700

    Added Teacher python
    Added the teacher python. Used current_user.courses relationship to get the courses of the current_user

---
+++ website/site_teacher.py
@@ -0,0 +1,102 @@
+from werkzeug.security import generate_password_hash as gph
+from werkzeug.security import check_password_hash as cph
+from flask import Blueprint, render_template, request, abort, Response, flash, redirect, url_for
+from flask_login import current_user
+from urllib.parse import unquote
+from functools import wraps
+from .models import User, Course
+
+site_teacher = Blueprint("site_teacher", __name__)
+
+def get_courses(request):
+    try:
+        page = request.args.get("page", 1)
+        page = int(page)
+    except:
+        page = 1
+    try:
+        per_page = request.args.get("per_page", 50)
+        per_page = int(per_page)
+    except:
+        per_page = 50
+
+    # what kind of search are we doing?
+    query = request.args.get("query", "name")
+    if(query not in ["name", "id", "dept"]):
+        query = "name"
+    # specific term?
+    term = request.args.get("term", None)
+    
+    courses = Course.query
+    if(term):
+        courses = courses.filter_by(term=term)
+
+    # fuzzy search by name
+    if(query == "name"):
+        course_name = request.args.get("name", "")
+        if(course_name):
+            courses = courses.filter(Course.name.like(f"%{course_name}%"))
+    # search by ID
+    elif(query == "id"):
+        course_id = request.args.get("id", None)
+        try:
+            course_id = int(course_id)
+        except:
+            abort(Response("Invalid course number.", 400))
+        if(not course_id):
+            abort(Response("ID is required.", 400))
+        courses = courses.filter_by(id=course_id)
+    # search by course department and number
+    else:
+        course_dept = request.args.get("dept", None)
+        course_num = request.args.get("num", None)
+        min_number = request.args.get("min", None)
+        max_number = request.args.get("max", None)
+        if(course_dept):
+            courses = courses.filter_by(course_dept=course_dept)
+        if(course_num is not None):
+            courses = courses.filter_by(number=course_num)
+        if(min_number):
+            courses = courses.filter(Course.number >= min_number)
+        if(max_number):
+            courses = courses.filter(Course.number <= max_number)
+        
+    # paginate
+    pagination = courses.paginate(page=page, per_page=per_page)
+    courses = pagination.items
+    total_pages = pagination.pages
+    total_courses = pagination.total
+
+    # return
+    return(courses, page, total_pages, total_courses, per_page)
+
+@site_teacher.route("/Courses")
+def courses():
+    
+    courses, page, total_pages, total_courses, per_page = get_courses(request)
+    rows = []
+
+    courses = current_user.courses
+
+    for course in courses:
+        rows.append([
+            course.id,
+            course.name,
+            course.dept,
+            course.number,
+            course.session,
+            course.units
+        ])
+
+    titles = ["ID", "Name", "Department", "Number", "Session", "Units", "Actions"]
+
+    return(render_template(
+        "teacher_courses.html",
+        courses=courses,
+        rows=rows,
+        titles=titles,
+        current_page=page,
+        total_pages=total_pages,
+        total_courses=total_courses,
+        items_per_page=per_page
+    ))
